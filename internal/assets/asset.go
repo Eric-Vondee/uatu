64b620431fef6303b33d137648f985e01b4a6dc2
Eric-Vondee <ericvondee5@gmail.com>
2026-08-11 20:26:24 +0100

    feat: add Agni and SushiSwap asset URLs to asset.go

--- internal/assets/asset.go
+++ internal/assets/asset.go
@@ -1,10 +1,12 @@
 package assets
 
 const (
+	Agni       = "https://pub-24d20d8084c94881864eeeb6799e564e.r2.dev/dex/agni.jpg"
 	Aerodrome  = "https://pub-24d20d8084c94881864eeeb6799e564e.r2.dev/dex/aerodrome.png"
 	CowSwap    = "https://pub-24d20d8084c94881864eeeb6799e564e.r2.dev/dex/cowswap.png"
 	Oku        = "https://pub-24d20d8084c94881864eeeb6799e564e.r2.dev/dex/oku.jpeg"
 	QuickSwap  = "https://pub-24d20d8084c94881864eeeb6799e564e.r2.dev/dex/quickswap.svg"
 	PancakSwap = "https://pub-24d20d8084c94881864eeeb6799e564e.r2.dev/dex/pancakeswap.jpeg"
+	SushiSwap  = "https://pub-24d20d8084c94881864eeeb6799e564e.r2.dev/dex/sushiswap.jpeg"
 	Uniswap    = "https://pub-24d20d8084c94881864eeeb6799e564e.r2.dev/dex/uniswap.png"
 )
