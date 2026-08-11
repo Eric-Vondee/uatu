d39f1fcad9790a2197aae87a4bf709855286cee9
Eric-Vondee <ericvondee5@gmail.com>
2026-08-11 23:58:44 +0100

    feat: add blockchain_logo column to blockchains table

--- internal/storage/postgres/migrations/000002_create_blockchains_table.up.sql
+++ internal/storage/postgres/migrations/000002_create_blockchains_table.up.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS blockchains (
     symbol VARCHAR(100) NOT NULL,
     chain_id BIGINT UNIQUE NOT NULL,
     block_explorer_url VARCHAR(255) NOT NULL,
+    blockchain_logo VARCHAR(255),
     native_token VARCHAR(255) NOT NULL,
     tokens JSONB NOT NULL DEFAULT '[]'::jsonb,
     dex JSONB NOT NULL DEFAULT '[]'::jsonb,
